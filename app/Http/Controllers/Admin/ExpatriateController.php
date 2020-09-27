134e620c58fb4883887f1d3390025e77f9b8197b
Md Humayun Kabir <humayun.kabir@btraccl.com>
2020-09-27 12:23:23 +0600

    visa and arrival info added

--- app/Http/Controllers/Admin/ExpatriateController.php
+++ app/Http/Controllers/Admin/ExpatriateController.php
@@ -64,7 +64,7 @@ class ExpatriateController extends ApiController
          return view('admin.expatriate.create', compact('religion','gender','countries','country'));
     }
 
-    public function addBasicInfo(Request $request)
+    public function store(Request $request)
     {
 
 
@@ -72,16 +72,11 @@ class ExpatriateController extends ApiController
             "passport_number"         => "required",
             "passport_expiry_date"          => "required",
             "first_name"          => "required",
+            "visa_type"          => "required",
+            "visa_expiry_date"          => "required",
         ];
-        //$request->validate($rules);
+        $request->validate($rules);
 
-        $validator = Validator::make($request->all(), $rules);
-        if ($validator->fails())
-        {
-            // if validation fail
-            $errors = $validator->messages()->toArray();
-            return $this->respondWithValidationError($errors);
-        }
 
         /**
          * Check is passport Number exist
@@ -90,121 +85,20 @@ class ExpatriateController extends ApiController
 
         if($isExist||$isExist>0)
         {
-            $this->respondWithError('This person is already exist');
-//            session()->flash('message', 'This passport already exist in the system');
-//            session()->flash('class', '2');
-//            return redirect()->route('education_create');
-        }
-
-        /**
-         * Get basic info data from request
-         */
-
-        $basic_data= $request->only([
-                'first_name',
-                'last_name',
-                'father_name',
-                'mother_name',
-                'marital_status',
-                'spouse_name',
-                'nid',
-                'nationality',
-                'date_of_birth',
-                'birth_country_id',
-                'gender',
-                'religion_id',
-                'email',
-                'mobile',
-                'passport_number',
-                'passport_issue_date',
-                'passport_expiry_date',
-                'passport_issue_place',
-                'facebook_id',
-                'linkedin_id',
-                'line_id',
-                'whatsapp_id'
-            ]);
-
-        $basic_data['active_status']=1;
-        $basic_data['created_by']=Auth::id();
-        $basic_data['created_at']=date('Y-m-d H:i:s');
-
-        /**
-         * Insert Into Expatriate table and get ID
-         */
-
-        $id = Expatriate::insertGetId($basic_data);
-
-        /**
-         * Passport image uploading
-         */
-
-        $passport_img_path=null;
-        if($request->hasFile('image'))
-        {
-            $passport_img_path = '';
-            $path = public_path('uploads/passport_images/passport');
-            if(!File::exists($path)) {
-                File::makeDirectory($path, $mode = 0777, true, true);
-            }
-
-            $image_name = time().'.'.$request->image->getClientOriginalExtension();
-
-            if($image_name){
-                request()->image->move($path, $image_name);
-                $passport_img_path = "uploads/passport_images/passport/" . $image_name;
-            }
-        }
-        $passport_data['expat_id']=$id;
-        $passport_data['image']=$passport_img_path;
-        $passport_data['passport_number'] = $basic_data['passport_number'];
-        $passport_data['passport_issue_date'] = $basic_data['passport_issue_date'];
-        $passport_data['passport_expiry_date'] = $basic_data['passport_expiry_date'];
-        $passport_data['passport_issue_place'] = $basic_data['passport_issue_place'];
-        $passport_data['active_status']=1;
-        $passport_data['created_by']=Auth::id();
-        $passport_data['created_at']=date('Y-m-d H:i:s');
-
-        Passport::insert($passport_data);
-
-        $expatriate_info =Expatriate::find($id);
-
-        return $this->respondWithSuccess('Inserted Successfully',$expatriate_info);
-
-    }
-
-
-    public function addVisaInfo(Request $request)
-    {
-
-        $rules =[
-            "visa_type"   => "required",
-            "expiry_date"       => "required",
-            "first_name"        => "required",
-            "expat_id"          => "required",
-        ];
-        //$request->validate($rules);
-
-        $validator = Validator::make($request->all(), $rules);
-        if ($validator->fails())
-        {
-            // if validation fail
-            $errors = $validator->messages()->toArray();
-            return $this->respondWithValidationError($errors);
-        }
-
-        /**
-         * Check is passport Number exist
-         */
-        $isExist     =VisaInfo::where('passport_number',$request->input('passport_number'))->where('active_status',1)->count();
-
-        if(!$isExist)
-        {
             session()->flash('message', 'This passport already exist in the system');
             session()->flash('class', '2');
             return redirect()->route('education_create');
         }
 
+        $expat_id = $this->processBasicInfo($request,1);
+        $this->processPassport($request,$expat_id,1);
+        $this->processVisa($request,$expat_id,1);
+        $this->processArrival($request,$expat_id,1);
+
+    }
+
+    private function processBasicInfo($request,$type=1,$id=null)
+    {
         /**
          * Get basic info data from request
          */
@@ -235,35 +129,349 @@ class ExpatriateController extends ApiController
         ]);
 
         $basic_data['active_status']=1;
-        $basic_data['created_by']=Auth::id();
-        $basic_data['created_at']=date('Y-m-d H:i:s');
 
-        /**
-         * Insert Into Expatriate table and get ID
-         */
-
-        $id = Expatriate::insertGetId($basic_data);
-
-        /**
-         * Insert passport data
-         */
-        $passport_data['expat_id']=$id;
-        $passport_data['passport_number'] = $basic_data['passport_number'];
-        $passport_data['passport_issue_date'] = $basic_data['passport_issue_date'];
-        $passport_data['passport_expiry_date'] = $basic_data['passport_expiry_date'];
-        $passport_data['passport_issue_place'] = $basic_data['passport_issue_place'];
-        $passport_data['active_status']=1;
-        $passport_data['created_by']=Auth::id();
-        $passport_data['created_at']=date('Y-m-d H:i:s');
-
-        Passport::insert($passport_data);
-
-        $expatriate_info =Expatriate::find($id);
-
-        return $this->respondWithSuccess('Inserted Successfully',$expatriate_info);
+        if($type==1)
+        {
+            //Insert
+            $basic_data['created_by']=Auth::id();
+            $basic_data['created_at']=date('Y-m-d H:i:s');
+            return Expatriate::insertGetId($basic_data);
+        }else{
+            //Update
+            $basic_data['updated_by']=Auth::id();
+            $basic_data['updated_at']=date('Y-m-d H:i:s');
+            Expatriate::where('id',$id)->update($basic_data);
+        }
 
 
     }
+
+    private function processPassport($request,$expat_id,$type=1,$id=null)
+    {
+
+        if($request->hasFile('passport_file'))
+        {
+            $img_path = $this->uploadFile($request,'passport_file','passport');
+            $items['passport_file']=$img_path;
+        }
+
+        $items['expat_id']=$expat_id;
+        $items['passport_number'] = $request->input('passport_number');
+        $items['passport_issue_date'] = $request->input('passport_issue_date');
+        $items['passport_expiry_date'] = $request->input('passport_expiry_date');
+        $items['passport_issue_place'] = $request->input('passport_issue_place');
+
+        if($type==1)
+        {
+            //Insert data
+            $items['active_status']=1;
+            $items['created_by']=Auth::id();
+            $items['created_at']=date('Y-m-d H:i:s');
+            return VisaInfo::insert($items);
+        }else{
+            //Update data
+            // $items['active_status']=1;
+            $items['updated_by']=Auth::id();
+            $items['updated_at']=date('Y-m-d H:i:s');
+            return VisaInfo::where('id',$id)->update($items);
+        }
+
+    }
+
+    private function processVisa($request,$expat_id,$type=1,$id=null)
+    {
+
+        if($request->hasFile('visa_file'))
+        {
+            $img_path = $this->uploadFile($request,'visa_file','visa');
+            $items['visa_file']=$img_path;
+        }
+
+        $items['expat_id']=$expat_id;
+        $items['visa_type']=$request->input('visa_type');
+        $items['issue_date']=$request->input('visa_issue_date');
+        $items['expiry_date']=$request->input('visa_expiry_date');
+        $items['entry_type']=$request->input('visa_entry_type');
+
+        if($type==1)
+        {
+            //Insert data
+            $items['active_status']=1;
+            $items['created_by']=Auth::id();
+            $items['created_at']=date('Y-m-d H:i:s');
+           return VisaInfo::insert($items);
+        }else{
+            //Update data
+           // $items['active_status']=1;
+            $items['updated_by']=Auth::id();
+            $items['updated_at']=date('Y-m-d H:i:s');
+           return VisaInfo::where('id',$id)->update($items);
+        }
+
+    }
+
+    private function processArrival($request,$expat_id,$type=1,$id=null)
+    {
+        if($request->hasFile('arrival_immigration_endorsement_file'))
+        {
+            $img_path = $this->uploadFile($request,'arrival_immigration_endorsement_file','immigration_endorsement');
+            $items['immigration_endorsement_file']=$img_path;
+        }
+
+        $items['expat_id']=$expat_id;
+        $items['travel_type']=$request->input('travel_type');
+        $items['arrival_country_id']=$request->input('arrival_country_id');
+        $items['date']=date('Y-m-d',strtotime($request->input('arrival_date')));
+        $items['iata_code']=$request->input('arrival_iata_code');
+        $items['immigration_endorsement_date']=date('Y-m-d',strtotime($request->input('immigration_endorsement_date')));
+
+
+        if($type==1)
+        {
+            //Insert data
+            $items['active_status']=1;
+            $items['created_by']=Auth::id();
+            $items['created_at']=date('Y-m-d H:i:s');
+            return VisaInfo::insert($items);
+        }else{
+            //Update data
+            // $items['active_status']=1;
+            $items['updated_by']=Auth::id();
+            $items['updated_at']=date('Y-m-d H:i:s');
+            return VisaInfo::where('id',$id)->update($items);
+        }
+
+    }
+
+
+
+
+
+    
+    private function uploadFile($request,$file_name,$folder_name)
+    {
+
+        $passport_img_path = '';
+        $path = public_path('uploads/'.$folder_name);
+        if(!File::exists($path)) {
+            File::makeDirectory($path, $mode = 0777, true, true);
+        }
+
+        $image_name = time().'.'.$request->$file_name->getClientOriginalExtension();
+
+        if($image_name){
+            request()->image->move($path, $image_name);
+            $passport_img_path = "uploads/".$folder_name."/" . $image_name;
+        }
+
+        return $passport_img_path;
+
+    }
+
+
+
+//    public function store(Request $request)
+//    {
+//
+//
+//        $rules =[
+//            "passport_number"         => "required",
+//            "passport_expiry_date"          => "required",
+//            "first_name"          => "required",
+//            "visa_type"          => "required",
+//            "visa_expiry_date"          => "required",
+//        ];
+//        //$request->validate($rules);
+//
+//        $validator = Validator::make($request->all(), $rules);
+//        if ($validator->fails())
+//        {
+//            // if validation fail
+//            $errors = $validator->messages()->toArray();
+//            return $this->respondWithValidationError($errors);
+//        }
+//
+//        /**
+//         * Check is passport Number exist
+//         */
+//        $isExist     =Expatriate::where('passport_number',$request->input('passport_number'))->where('active_status',1)->count();
+//
+//        if($isExist||$isExist>0)
+//        {
+//            //$this->respondWithError('This person is already exist');
+//            session()->flash('message', 'This passport already exist in the system');
+//            session()->flash('class', '2');
+//            return redirect()->route('education_create');
+//        }
+//
+//        /**
+//         * Get basic info data from request
+//         */
+//
+//        $basic_data= $request->only([
+//            'first_name',
+//            'last_name',
+//            'father_name',
+//            'mother_name',
+//            'marital_status',
+//            'spouse_name',
+//            'nid',
+//            'nationality',
+//            'date_of_birth',
+//            'birth_country_id',
+//            'gender',
+//            'religion_id',
+//            'email',
+//            'mobile',
+//            'passport_number',
+//            'passport_issue_date',
+//            'passport_expiry_date',
+//            'passport_issue_place',
+//            'facebook_id',
+//            'linkedin_id',
+//            'line_id',
+//            'whatsapp_id'
+//        ]);
+//
+//        $basic_data['active_status']=1;
+//        $basic_data['created_by']=Auth::id();
+//        $basic_data['created_at']=date('Y-m-d H:i:s');
+//
+//        /**
+//         * Insert Into Expatriate table and get ID
+//         */
+//
+//        $id = Expatriate::insertGetId($basic_data);
+//
+//        /**
+//         * Passport image uploading
+//         */
+//
+//        $passport_img_path=null;
+//        if($request->hasFile('image'))
+//        {
+//            $passport_img_path = '';
+//            $path = public_path('uploads/passport_images/passport');
+//            if(!File::exists($path)) {
+//                File::makeDirectory($path, $mode = 0777, true, true);
+//            }
+//
+//            $image_name = time().'.'.$request->image->getClientOriginalExtension();
+//
+//            if($image_name){
+//                request()->image->move($path, $image_name);
+//                $passport_img_path = "uploads/passport_images/passport/" . $image_name;
+//            }
+//        }
+//        $passport_data['expat_id']=$id;
+//        $passport_data['image']=$passport_img_path;
+//        $passport_data['passport_number'] = $basic_data['passport_number'];
+//        $passport_data['passport_issue_date'] = $basic_data['passport_issue_date'];
+//        $passport_data['passport_expiry_date'] = $basic_data['passport_expiry_date'];
+//        $passport_data['passport_issue_place'] = $basic_data['passport_issue_place'];
+//        $passport_data['active_status']=1;
+//        $passport_data['created_by']=Auth::id();
+//        $passport_data['created_at']=date('Y-m-d H:i:s');
+//
+//        Passport::insert($passport_data);
+//
+//        $expatriate_info =Expatriate::find($id);
+//
+//        return $this->respondWithSuccess('Inserted Successfully',$expatriate_info);
+//
+//    }
+
+
+//    public function addVisaInfo(Request $request)
+//    {
+//
+//        $rules =[
+//            "visa_type"   => "required",
+//            "expiry_date"       => "required",
+//            "first_name"        => "required",
+//            "expat_id"          => "required",
+//        ];
+//        //$request->validate($rules);
+//
+//        $validator = Validator::make($request->all(), $rules);
+//        if ($validator->fails())
+//        {
+//            // if validation fail
+//            $errors = $validator->messages()->toArray();
+//            return $this->respondWithValidationError($errors);
+//        }
+//
+//        /**
+//         * Check is passport Number exist
+//         */
+//        $isExist     =VisaInfo::where('passport_number',$request->input('passport_number'))->where('active_status',1)->count();
+//
+//        if(!$isExist)
+//        {
+//            session()->flash('message', 'This passport already exist in the system');
+//            session()->flash('class', '2');
+//            return redirect()->route('education_create');
+//        }
+//
+//        /**
+//         * Get basic info data from request
+//         */
+//
+//        $basic_data= $request->only([
+//            'first_name',
+//            'last_name',
+//            'father_name',
+//            'mother_name',
+//            'marital_status',
+//            'spouse_name',
+//            'nid',
+//            'nationality',
+//            'date_of_birth',
+//            'birth_country_id',
+//            'gender',
+//            'religion_id',
+//            'email',
+//            'mobile',
+//            'passport_number',
+//            'passport_issue_date',
+//            'passport_expiry_date',
+//            'passport_issue_place',
+//            'facebook_id',
+//            'linkedin_id',
+//            'line_id',
+//            'whatsapp_id'
+//        ]);
+//
+//        $basic_data['active_status']=1;
+//        $basic_data['created_by']=Auth::id();
+//        $basic_data['created_at']=date('Y-m-d H:i:s');
+//
+//        /**
+//         * Insert Into Expatriate table and get ID
+//         */
+//
+//        $id = Expatriate::insertGetId($basic_data);
+//
+//        /**
+//         * Insert passport data
+//         */
+//        $passport_data['expat_id']=$id;
+//        $passport_data['passport_number'] = $basic_data['passport_number'];
+//        $passport_data['passport_issue_date'] = $basic_data['passport_issue_date'];
+//        $passport_data['passport_expiry_date'] = $basic_data['passport_expiry_date'];
+//        $passport_data['passport_issue_place'] = $basic_data['passport_issue_place'];
+//        $passport_data['active_status']=1;
+//        $passport_data['created_by']=Auth::id();
+//        $passport_data['created_at']=date('Y-m-d H:i:s');
+//
+//        Passport::insert($passport_data);
+//
+//        $expatriate_info =Expatriate::find($id);
+//
+//        return $this->respondWithSuccess('Inserted Successfully',$expatriate_info);
+//
+//
+//    }
 
 
 
